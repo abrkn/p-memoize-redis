1e69120ab2aee3d74e9a7d3df3caf614a029f924
Andreas Brekken <andreas@brekken.com>
2018-08-31 16:25:24 +0700

    Add prefix

--- index.js
+++ index.js
@@ -16,30 +16,22 @@ const PREFIX = 'mem.';
 //
 // Options:
 //  ttl: Expiration time in seconds. Default: 60
-module.exports = function createRedisMemCache(redisUrl) {
+module.exports = function createRedisMemCache(redisUrl, prefix = '') {
   const redisClient = redis.createClient(redisUrl);
 
-  const asBase64 = value =>
-    new Buffer(JSON.stringify(value)).toString('base64');
-  const withPrefix = key => `${PREFIX}${asBase64(key)}`;
+  const asBase64 = value => new Buffer(JSON.stringify(value)).toString('base64');
+  const withPrefix = key => `${PREFIX}${prefix}.${asBase64(key)}`;
   const has = key => redisClient.existsAsync(withPrefix(key));
-  const get = key =>
-    redisClient.getAsync(withPrefix(key)).then(_ => (_ ? JSON.parse(_) : _));
+  const get = key => redisClient.getAsync(withPrefix(key)).then(_ => (_ ? JSON.parse(_) : _));
   const set = async (key, { data, maxAge }) => {
     const resolved = await data;
     const ttl = maxAge - +new Date();
-    return redisClient.setAsync(
-      withPrefix(key),
-      JSON.stringify(resolved),
-      'PX',
-      ttl
-    );
+    return redisClient.setAsync(withPrefix(key), JSON.stringify(resolved), 'PX', ttl);
   };
 
   const del = key => redisClient.delAsync(withPrefix(key));
   const getAllKeys = () => redisClient.keysAsync(withPrefix('*'));
-  const clear = () =>
-    getAllKeys.then(keys => Promise.all(key => redisClient.delAsync(key)));
+  const clear = () => getAllKeys.then(keys => Promise.all(key => redisClient.delAsync(key)));
 
   return { has, get, set, delete: del, clear };
 };
